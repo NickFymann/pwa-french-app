8b68cb57b96c942f83f0ebdace9af5be7753de3a
Nikita Yefimenko (local) <nikitaefimenkko@gmail.com>
2025-07-07 16:25:56 +0300

    manifest.js and index.html changed, service-worker.js added,

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_VERSION = 'v1'; 
+const CACHE_NAME = `pwa-cache-${CACHE_VERSION}`;
+
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './rules.css',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
